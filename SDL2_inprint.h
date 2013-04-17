1c193ca038261158b8fe003bd8622418150e625c
Vitaly Driedfruit <driedfruit@mindloop.net>
2013-04-17 08:25:07 +0400

    Fix bug in previous commit.

--- SDL2_inprint.h
+++ SDL2_inprint.h
@@ -7,7 +7,7 @@ extern void prepare_inline_font(void);
 extern void kill_inline_font(void);
 
 extern void inrenderer(SDL_Renderer *renderer);
-extern void infont(SDL_Renderer *renderer, SDL_Texture *font);
+extern void infont(SDL_Texture *font);
 extern void incolor1(SDL_Color *color);
 extern void incolor(Uint32 color, Uint32 unused); /* Color must be in 0x00RRGGBB format ! */
 extern void inprint(SDL_Renderer *dst, const char *str, Uint32 x, Uint32 y);
